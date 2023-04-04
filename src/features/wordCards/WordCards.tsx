9d525782cf3641276bf8b9ee071db6b8afb2ac11
Rowadz <rowadwo@gmail.com>
2023-04-04 20:25:30 +0200

    remove log

--- src/features/wordCards/WordCards.tsx
+++ src/features/wordCards/WordCards.tsx
@@ -6,7 +6,6 @@ import { selectWords, WordsState } from './wordCardsSlice'
 
 export const WordCards = memo(() => {
   const words: WordsState = useSelector(selectWords)
-  console.log({ words })
 
   return (
     <div className="flex justify-evenly	flex-wrap ">
