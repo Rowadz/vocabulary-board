caebd3730e3030310ee0c68a0eec539be429ea54
Rowadz <rowadwo@gmail.com>
2023-04-04 20:25:56 +0200

    add shallow equal

--- src/features/wordCards/WordCards.tsx
+++ src/features/wordCards/WordCards.tsx
@@ -1,11 +1,11 @@
 import { memo } from 'react'
-import { useSelector } from 'react-redux'
+import { useSelector, shallowEqual } from 'react-redux'
 import { DefinitionAPIResponse } from '../../services/types'
 import { WordCard } from './components/WordCard'
 import { selectWords, WordsState } from './wordCardsSlice'
 
 export const WordCards = memo(() => {
-  const words: WordsState = useSelector(selectWords)
+  const words: WordsState = useSelector(selectWords, shallowEqual)
 
   return (
     <div className="flex justify-evenly	flex-wrap ">
