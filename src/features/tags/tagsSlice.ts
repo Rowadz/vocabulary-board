22d74c932cd81077d28cc8370379093dfdea2c5b
Rowadz <rowadwo@gmail.com>
2023-04-09 16:39:11 +0200

    save tags when adding them to the locale storage

--- src/features/tags/tagsSlice.ts
+++ src/features/tags/tagsSlice.ts
@@ -1,8 +1,14 @@
-import { createEntityAdapter, createSlice } from '@reduxjs/toolkit'
+import {
+  createEntityAdapter,
+  createSlice,
+  EntityState,
+  PayloadAction,
+} from '@reduxjs/toolkit'
 import { RootState } from '../../app/store'
 
 export type Tag = { tagId: string; title: string }
 
+const KEY = 'vocabulary-board-tags'
 const tagsAdapter = createEntityAdapter<Tag>({
   selectId: (book) => book.tagId,
   sortComparer: (a, b) => a.title.localeCompare(b.title),
@@ -10,9 +16,17 @@ const tagsAdapter = createEntityAdapter<Tag>({
 
 const tagsSlice = createSlice({
   name: 'tags',
-  initialState: tagsAdapter.getInitialState(),
+  initialState: tagsAdapter.getInitialState(
+    JSON.parse(localStorage.getItem(KEY) || '{}') as EntityState<Tag>
+  ),
   reducers: {
-    addTag: tagsAdapter.addOne,
+    addTag: (
+      state: EntityState<Tag>,
+      { payload: { tagId, title } }: PayloadAction<Tag>
+    ) => {
+      tagsAdapter.addOne(state, { tagId, title })
+      localStorage.setItem(KEY, JSON.stringify(state))
+    },
   },
 })
 
